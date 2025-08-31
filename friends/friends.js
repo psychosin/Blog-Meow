5f51a6a6034c53074f94f658b989863f863a3374
psychosin <myworldxi@foxmail.com>
2025-08-31 12:34:33 +0800

    Site updated: 2025-08-31 12:34:32

---
+++ friends/friends.js
@@ -0,0 +1,39 @@
+const friendsData = {
+  friends: [
+    {
+      class: {
+        name: '小伙伴们',
+        desc: '这个分类下的友链配置了随机排列',
+        random: true
+      },
+      list: [
+        {
+          name: '000',
+          url: 'https://chanwj.github.io',
+          avatar: 'assets/images/Hexo-Theme-MEOW.png',
+          desc: '0000000000000000000'
+        },
+        {
+          name: '1111',
+          url: 'https://chanwj.github.io',
+          avatar: 'assets/images/Hexo-Theme-MEOW.png',
+          desc: '1111111111111111'
+        },
+        {
+          name: '222',
+          url: 'https://chanwj.github.io',
+          avatar: 'assets/images/Hexo-Theme-MEOW.png',
+          desc: '222222222222222222'
+        },
+      ]
+    },
+    {
+      class: {
+        name: '第二个分类',
+        desc: '分类描述……',
+        random: false
+      },
+      list: []
+    },
+  ]
+};
